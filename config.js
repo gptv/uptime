3c750143aa40ed83d0caed3867865aec1eb4e910
gpter <admin@i.pkuschool.edu.cn>
2024-10-11 21:06:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,6 @@
 window.Config = {
-
-  // 显示标题
   SiteName: 'Uptime-status',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'ur2702718-d892dbb7cba75cc02737f08c',
-  ],
-
-  // 日志天数
+  ApiKeys: ['ur2720718-9d755ba29d53ba4b16e63a70'],
   CountDays: 60,
-
-  // 是否显示检测站点的链接
   ShowLink: true,
-  
 };
-
-window.Config = {
